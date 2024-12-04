f55f5b1fe603f3a5507fbd7473265cd503f865bb
Ayann12 <tantowi818@gmail.com>
2024-12-04 09:16:52 +0700

    first commit

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const primary = Color(0xFF2c3E50);
+  static const secondary = Color(0xFFE74C3c);
+  static const accent = Color(0xff3498DB);
+  static const background = Color(0xffF8F9FA);
+  static const CardBg = Colors.white;
+  static const TextDark = Color(0xff2C3E50);
+  static const TextLight = Color(0xff95A5A6);
+  static const Success = Color(0xff2ECC71);
+}
